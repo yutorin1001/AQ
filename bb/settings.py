2b59d835c520ccb361af7c6b94eea2216565d8f1
yutorin1001 <atmtwn@outlook.jp>
2025-06-15 19:13:27 +0000

    2

--- bb/settings.py
+++ bb/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = False
 
 
+SECRET_KEY = 'django-insecure--s62ea(cr_t@b*%1d$*ig&7+lc31z15g$uu**3ma4a-dcw2)=z'
 
 
 
---
+++ local_setting.py
@@ -0,0 +1,2 @@
+DEBUG = True
+SECRET_KEY = 'django-insecure--s62ea(cr_t@b*%1d$*ig&7+lc31z15g$uu**3ma4a-dcw2)=z'
